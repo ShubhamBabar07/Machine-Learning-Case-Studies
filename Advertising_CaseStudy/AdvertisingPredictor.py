22ae73a1b6d1e65363cf96718baac78766459c02
Shubham Babar <babarshubham133@gmail.com>
2024-08-12 16:42:35 +0530

    Advertising Case Study

---
+++ Advertising_CaseStudy/AdvertisingPredictor.py
@@ -0,0 +1,48 @@
+# Advertising Predictor by Linear Regression
+
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+def MarvellousAdvertisementPredictor(data_path):
+
+    data = pd.read_csv(data_path, index_col=0)
+
+    print("Size of Actual dataset :", len(data))
+
+    feature_names = ['TV', 'radio', 'newspaper']
+
+    print("\nNames of Features :", feature_names)
+
+    X = data[feature_names]
+    y = data.sales
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/2)
+
+    print("\nSize of Training dataset :", len(X_train))
+    print("\nSize of Testing dataset :", len(X_test))
+
+    linreg = LinearRegression()
+
+    linreg.fit(X_train, y_train)
+
+    y_pred = linreg.predict(X_test)
+
+    print("\nTesting set :")
+    print(X_test)
+
+    print("\nResult of Testing : ")
+    print(y_pred)
+
+    print("\nmean_squared_error : ",np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+def main():
+
+    MarvellousAdvertisementPredictor("Advertising.csv")
+
+
+if __name__ == "__main__":
+    main()
